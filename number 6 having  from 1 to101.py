d6ac76f7025fe13b6c725d274b52231b130b906c
S N KISHORE KUMAR <121159219+Kishorekumar9951@users.noreply.github.com>
2022-12-27 16:45:52 +0530

    Add files via upload

---
+++ number 6 having  from 1 to101.py
@@ -0,0 +1,9 @@
+##lst=[]
+##for i in range(1,101):
+##    if '6' in str(i):
+##        lst.append(i)
+##print(lst)
+
+lst=[i for i in range(1,101) if '6' in str(i)]
+print(lst)
+
---
+++ vowels from an string.py
@@ -0,0 +1,26 @@
+##string=input()
+##ans=[]
+##for i in string:
+##    if i in 'AEIOUaeiou':
+##        ans.append(i)
+##print(*ans)
+##
+##ans=[]
+##print(len(ans))
+
+
+##string='kishore'
+##ans=[i for i in string if i in 'AEIOUaeiou']
+##print(*ans)
+
+##string='kishore1234'
+##ans=[i for i in string if i in '0123456789']
+##print(*ans)
+
+##lst=[i for i in range(1,1001)]
+##print(*lst)
+
+##string='jasmine'
+##lst=[i for i in string if i not in 'AEIOUaeiou']
+##print(*lst)
+
